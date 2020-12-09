cba2444bbcb46ba563659fdb7aa6bfaa267e9fb4
AndreaPontrandolfo <andrea.pontra@gmail.com>
2020-12-09 23:35:45 +0100

    added Pie

---
+++ src/mock.js
@@ -0,0 +1,32 @@
+export const mock = [
+  {
+    id: "python",
+    label: "python",
+    value: 479,
+    color: "hsl(92, 70%, 50%)",
+  },
+  {
+    id: "ruby",
+    label: "ruby",
+    value: 346,
+    color: "hsl(9, 70%, 50%)",
+  },
+  {
+    id: "javascript",
+    label: "javascript",
+    value: 117,
+    color: "hsl(301, 70%, 50%)",
+  },
+  {
+    id: "elixir",
+    label: "elixir",
+    value: 474,
+    color: "hsl(252, 70%, 50%)",
+  },
+  {
+    id: "erlang",
+    label: "erlang",
+    value: 17,
+    color: "hsl(233, 70%, 50%)",
+  },
+];
