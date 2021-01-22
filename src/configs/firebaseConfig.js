f53a004f9b4b9c3b388d8145eb812d56789e8ebb
AndreaPontrandolfo <andrea.pontra@gmail.com>
2021-01-23 00:19:29 +0100

    deploying config

---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkGvhAvk-yFKz3RmTvGYyN1U19G7TtUok",
+  authDomain: "portfolio-visualizer.firebaseapp.com",
+  databaseURL:
+    "https://portfolio-visualizer-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "portfolio-visualizer",
+  storageBucket: "portfolio-visualizer.appspot.com",
+  messagingSenderId: "502511022620",
+  appId: "1:502511022620:web:95823de6b78397df2565d9",
+};
