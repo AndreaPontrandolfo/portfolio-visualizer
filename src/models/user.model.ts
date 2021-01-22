4cd69965f92ea103d089106cfe1c842fd1e57c36
AndreaPontrandolfo <andrea.pontra@gmail.com>
2021-01-22 23:48:44 +0100

    removed some info

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,16 +1,29 @@
 // @ts-nocheck
 
-import { action } from "easy-peasy";
+import { action, thunk } from "easy-peasy";
 import { any } from "ramda";
-
-const passcodes = ["andrea", "boris", "francesco", "kalos"];
+import { database } from "../services/database";
 
 export const userModel = {
   user: "",
-  verifyUser: action((state, payload) => {
-    const matchAPasscode = (name) => name === payload;
+  verifyUser: thunk(async (actions, payload) => {
+    try {
+      const passcodesRef = database.ref("passcodes");
+      passcodesRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        actions.setAuthenticatedUser({ passcodes: data, username: payload });
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: user.model.ts ~ line 27 ~ verifyUser:thunk ~ error",
+        error
+      );
+    }
+  }),
+  setAuthenticatedUser: action((state, { passcodes, username }) => {
+    const matchAPasscode = (name) => name === username;
     if (any(matchAPasscode)(passcodes)) {
-      state.user = payload;
+      state.user = username;
     }
   }),
   selectedProfile: "",
